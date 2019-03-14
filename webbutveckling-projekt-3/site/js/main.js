55ca72e4af91d243c765bf3e5c63a5721e65b8fb
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2019-03-14 22:42:15 +0100

    Update main.js

--- webbutveckling-projekt-3/site/js/main.js
+++ webbutveckling-projekt-3/site/js/main.js
@@ -1,7 +1,1 @@
-// function buybar(){
-//    let cart_box = document.querySelector(".cart_box")
-//     cart_box.classList.toggle("activate")
-
-//     let carticons = document.querySelector(".carticons")
-//     carticons.classList.toggle("activate_carticons")
-// }
+ 
