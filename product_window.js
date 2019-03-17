857e6badbdea7491465b60ec3ddf23cf5110c14a
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2019-03-17 19:56:07 +0100

    Create product_window.js

---
+++ product_window.js
@@ -0,0 +1,8 @@
+function new_product(){
+    div = document.createElement("div")
+    div.classList.add("buy_box")
+    document.getElementById("products").appendChild(div)
+    
+    div = document.createElement("div")
+
+}
