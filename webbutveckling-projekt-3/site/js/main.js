df922e2e6eec26b509b5dcc62a1bacd1ee532183
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2019-03-25 16:57:54 +0100

    Update main.js

--- webbutveckling-projekt-3/site/js/main.js
+++ webbutveckling-projekt-3/site/js/main.js
@@ -1,6 +1,14 @@
 function a_menu(){
     burger_menu = document.querySelector(".burger_menu")
     burger_menu.classList.toggle("a_burger_menu")
+    monacle = document.getElementById("menu")
+    
+    if (monacle.innerHTML === "menu"){
+        monacle.innerHTML = "chevron_left"
+    }
+    else{
+        monacle.innerHTML = "menu"
+    }
 } 
 
 function goBack(){
